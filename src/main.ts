c7c35082cb0f899957760ce00fd2be483773ecf7
johnlin <john830110@gmail.com>
2026-01-02 20:25:00 +0800

    chore: 新增 GA4

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/main.css'
+import { createGtag } from "vue-gtag";
 
 const app = createApp(App)
 
+const gtag = createGtag({
+  tagId: import.meta.env.VITE_GA_ID,
+})
+
+app.use(gtag)
 app.mount('#app')
